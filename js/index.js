893f3a0e088b5fb116281a272069aa7c4df8c5c7
Yelena <88336329+DYA13@users.noreply.github.com>
2023-01-26 14:48:53 +0100

    add menu

--- js/index.js
+++ js/index.js
@@ -7,6 +7,14 @@ import menuLink from './menuLink.js';
 
 
 
-slideMenu('.header__burger-btn');
+slideMenu({
+    openBtn: '.header__burger-btn', 
+    menu:'.navigation',
+    classActiveMenu:'navigation_active',
+    closeTrigger:'.navigation__link, .navigation__close'
+
+});
 renderVideo();
 menuLink();
+
+
--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,30 @@
-const slideMenu = (openBtn, menu, classActiveMenu, closeTrigger) => {
+const openMenu = (nav, active) => {
+nav.classList.add(active);
+};
+
+const closeMenu = (nav, active) => {
+nav.classList.remove(active)
+};
+
+const slideMenu = ({openBtn, menu, classActiveMenu, closeTrigger} ) => {
+
+   
 
     const burgerBtn = document.querySelector(openBtn);
-    console.log('burgerBtn:', burgerBtn)
+    const navigation = document.querySelector(menu)
+    const navigationClose = document.querySelectorAll(closeTrigger);
+
+
+    burgerBtn.addEventListener('click', ()=>{
+        openMenu(navigation, classActiveMenu)
+    })
+  
+    navigationClose.forEach((item) => {
+        item.addEventListener('click', () => {
+            closeMenu(navigation, classActiveMenu)
+        })
+
+    })
 
 };
 
