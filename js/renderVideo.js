4a7e8229a64a9d450508a32e9e6c937a640254f1
Yelena Japarova <yelena.dzhaparova@gmail.com>
2023-06-05 10:59:36 +0200

    new file:   videotube/videotube.min.css,videotube/videotube.min.js

--- js/renderVideo.js
+++ js/renderVideo.js
@@ -1,48 +1,45 @@
 import { getTrends, getVideo } from "./services.js";
 import renderCard from "./renderCard.js";
 
-const filmWeek = document.querySelector('.film-week');
+const filmWeek = document.querySelector(".film-week");
 
-
-const firstRender = (data, keyVideo ) => {
-
-filmWeek.innerHTML =`
+const firstRender = (data, keyVideo) => {
+  filmWeek.innerHTML = `
 <div class="container film-week__container" 
    data-rating="${data.vote_average.toFixed(1)}">
 <div class="film-week__poster-wrapper">
     <img class="film-week__poster" 
-    src="https://www.themoviedb.org/t/p/w1920_and_h800_multi_faces${data.backdrop_path}" 
-    alt="постер ${data.name}">
-    <p class="film-week__title_origin">${data.original_name || data.original_title}</p>
+    src="https://www.themoviedb.org/t/p/w1920_and_h800_multi_faces${
+      data.backdrop_path
+    }" 
+    alt="poster ${data.name}">
+    <p class="film-week__title_origin">${
+      data.original_name || data.original_title
+    }</p>
 </div>
 <h2 class="film-week__title">${data.name || data.title}</h2>
-${keyVideo ? 
-   `<a class="film-week__watch-trailer tube" 
+${
+  keyVideo
+    ? `<a class="film-week__watch-trailer tube" 
    href="https://youtu.be/${keyVideo}" 
-   aria-label="смотреть трейлер"></a>` :
- ''}
+   aria-label="смотреть трейлер"></a>`
+    : ""
+}
 
 </div>
 `;
 };
 
 const renderVideo = async () => {
-   const data = await getTrends();
- 
+  const data = await getTrends();
 
-   const [ firstCard, ...otherCard ] = data.results;
-   otherCard.length = 16;
+  const [firstCard, ...otherCard] = data.results;
+  otherCard.length = 16;
 
-
-
-
-const video = await getVideo(firstCard.id, firstCard.media_type);
-
-
-
+  const video = await getVideo(firstCard.id, firstCard.media_type);
 
   firstRender(firstCard, video);
   renderCard(otherCard);
-}
+};
 
 export default renderVideo;
